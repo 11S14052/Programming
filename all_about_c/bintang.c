ddb5034ad5abd2215faa43859ba93e4722bc1063
Wahyu Adi P Nainggolan <wahyuhatake20@gmail.com>
2018-01-22 13:57:10 +0700

    create stars in c

--- all_about_c/bintang.c
+++ all_about_c/bintang.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+int main(){
+	
 
+int row,bintang,spasi;
+int n;
+ printf("masukkan banyaknya nilai n =");
+ scanf("%d",&n);
+ int banyakBintang = n;
+ for(row=1;row<=banyakBintang;row++){
+		 	for (spasi=1;spasi<=n;spasi++){
+		 		printf(" ");
+			 }
+			 for (bintang=1;bintang<=row;bintang++){
+			 	 printf(" ");
+				 printf("*");
+			 }
+			 printf("\n");
+			 n=n-1;
+ 		}
+ return 0;
+ }
+ 
+ 
